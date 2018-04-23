376189875573d87f2795560d1a07f850691d854a
Sid100Max <adanzsid@gmail.com>
2018-04-23 10:44:41 +0200

    make it better

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/dist'));
 app.use('/user',express.static(__dirname + '/dist'));
 app.use('/maps',express.static(__dirname + '/dist'));
+app.use('/questionpub',express.static(__dirname + '/dist'));
+app.use('/showprofil',express.static(__dirname + '/dist'));
 console.log ("gg");
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
